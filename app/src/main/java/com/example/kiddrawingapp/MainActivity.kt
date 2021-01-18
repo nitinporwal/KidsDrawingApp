87d2fc55f548b20711c2ac0a3a68e86cd929b02a
nitin-codr <nitinporwal99@gmail.com>
2021-01-18 14:08:31 +0530

    extract bitmap from canvas

--- app/src/main/java/com/example/kiddrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kiddrawingapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -159,6 +162,20 @@ class MainActivity : AppCompatActivity() {
     private fun isReadStorageAllowed(): Boolean {
         val result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
         return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getBitmapFromView(view: View) : Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        }
+        else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return returnedBitmap
     }
 
     companion object {
